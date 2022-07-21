634f217fd42f62a771d9011f012f426477f9cea6
unknown <dinoz64000@gmail.com>
2022-07-21 15:39:00 +0200

    push

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import ListeCours from "../components/ListeCours/listeCours";
 const Home = ({ cours }: { cours: cours[] }) => {
-  console.log(cours);
   return (
     <div className={styles.container}>
       <Head>
@@ -27,56 +26,8 @@ const Home = ({ cours }: { cours: cours[] }) => {
               description={cours.description}
             ></ListeCours>
           ))}
-          {/* <ListeCours
-            id="1"
-            name="Cours de React"
-            description="Cours sur les base de React"
-          />
-          <ListeCours
-            id="2"
-            name="Cours de React"
-            description="Cours sur les base de React"
-          />
-          <ListeCours
-            id="3"
-            name="Cours de Angular"
-            description="Cours sur les base de Angular"
-          />
-          <ListeCours
-            id="4"
-            name="Cours de React"
-            description="Cours sur les base de React"
-          />
-          <ListeCours
-            id="5"
-            name="Cours de React"
-            description="Cours sur les base de React"
-          />
-          <ListeCours
-            id="6"
-            name="Cours de React"
-            description="Cours sur les base de React"
-          /> */}
         </div>
       </main>
-
-      {/* <footer className={styles.footer}>
-        <a
-          href="https://www.cesi.fr/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image
-              src="/logo-cesi.png"
-              alt="Logo Cesi Petit"
-              width={72}
-              height={16}
-            />
-          </span>
-        </a>
-      </footer> */}
     </div>
   );
 };
